e461209c52c0d8b6e75f11780fea4ef84dbabf5c
Angelica <a.cortes.godoy@gmail.com>
2022-07-17 19:39:40 -0400

    add 3.2 and 3.3 html

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// 3.1
+
+function pintar() {
+    ele.style.backgroundColor = 'yellow'
+}
+ele = document.getElementById("ele1")
+ele.addEventListener("click", pintar);
+
+// 3.2 y 3.3
+
+// Verde por defecto
+
+        // div= document.querySelector('#ele1')
+        // function pintar(color= "green") {
+        //     div.style.backgroundColor = color
+        // }
+        // div.addEventListener("click", function () {pintar()});
+
+// Verde por defecto, al hacer click pasa a amarillo
+
+                // div= document.querySelector('#ele1')
+                // function pintar(color= "green") {
+                //     div.style.backgroundColor = color
+                // }
+                // div.addEventListener("click", function() {
+                //     pintar('yellow')
+                // });
